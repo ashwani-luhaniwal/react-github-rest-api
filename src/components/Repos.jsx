19a9c79c1750f2c995d812df3bdc76716c454199
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-08-21 13:04:48 -0700

    Add the functionality for sort

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -9,22 +9,19 @@ class Repos extends React.Component {
       };
     }
     componentDidMount() {
-      fetch(`https://api.github.com/users/${this.state.username}/repos?sort=full_name`)
+      fetch(`https://api.github.com/users/${this.state.username}/repos?sort=${this.props.location.query.sort}`)
       .then(response => response.json())
-      .then(
-        repos => {
-          this.setState({
-              repos: repos
-          });
-        }
-      );
+      .then(repos => {
+        this.setState({
+            repos: repos
+        });
+      });
     }
     render() {
       if (!this.state.repos) {
         return (<div className="repo-list">LOADING...</div>);
       }
-      const repos = this.state.repos;
-      const repoList = repos.map((repo) => {
+      const repoList = this.state.repos.map((repo) => {
         return (
           <Link className="list-group-item list-group-item-action" id={repo.id} data-toggle="list" to={`repos/${repo.name}`} role="tab" aria-controls={repo.name} key={repo.id}>
             {repo.name}
@@ -36,6 +33,13 @@ class Repos extends React.Component {
           <div className="navigation row">
             <a href="/" className="btn btn-default">Home</a>
             <a className="btn btn-default" onClick={browserHistory.goBack}>GitHub Username List</a>
+          </div>
+          <div className="sorted-by">
+            <strong>Sort by:</strong>
+            <a href={`/user/${this.state.username}/repos?sort=created`} className="btn btn-info sort-btn">Created</a>
+            <a href={`/user/${this.state.username}/repos?sort=updated`} className="btn btn-info sort-btn">Updated</a>
+            <a href={`/user/${this.state.username}/repos?sort=pushed`} className="btn btn-info sort-btn">Pushed</a>
+            <a href={`/user/${this.state.username}/repos?sort=full_name`} className="btn btn-info sort-btn">Full name</a>
           </div>
           <h4>List of all repositories</h4>
           <div className="row">
